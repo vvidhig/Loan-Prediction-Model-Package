3a413742f9103dca72a611115967da936bc45398
Vidhi Gupta <122139881+vvidhig@users.noreply.github.com>
2024-05-10 12:28:05 -0700

    adding the setup.py file

---
+++ setup.py
@@ -0,0 +1,52 @@
+import io
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+# Metadata of package
+NAME = 'prediction_model'
+DESCRIPTION = 'Loan Prediction Model'
+URL = 'https://github.com/vvidhig'
+EMAIL = 'vidhidoesnotreply@gmail.com'
+AUTHOR = 'Vidhi Gupta'
+REQUIRES_PYTHON = '>=3.9.0'
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+# Get the list of packages to be installed
+def list_reqs(fname='requirements.txt'):
+    with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
+        return f.read().splitlines()
+
+# Load the package's __version__.py module as a dictionary.
+ROOT_DIR = Path(__file__).resolve().parent
+PACKAGE_DIR = ROOT_DIR / NAME
+about = {}
+with open(PACKAGE_DIR / 'VERSION') as f:
+    _version = f.read().strip()
+    about['__version__'] = _version
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    package_data={'prediction_model': ['VERSION']},
+    install_requires=list_reqs(),
+    extras_require={},
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
